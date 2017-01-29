4d78e688ad8313d6af09ce8d247e79686e80eb83
Amanda Farrell <bluepandadev@gmail.com>
2017-01-28 19:15:10 -0700

    Initial commit

---
+++ app/src/main/java/com/example/amanda/scorekeeper/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.amanda.scorekeeper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseCount(view);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //SaveInstance prevents count from resetting when screen is rotated
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+        savedInstanceState.putInt("count", count);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+    //onRestoreInstanceState
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+        count = savedInstanceState.getInt("count");
+        displayCount(count);
+    }
+
+    int count = 0;
+
+    public void displayCount(int count) {
+        TextView countText = (TextView) findViewById(R.id.textbox);
+        countText.setText(String.valueOf(count));
+    }
+
+    public void increaseCount (View view) {
+        count +=1;
+        displayCount(count);
+    }
+}
+
+
+//Change icon of floating action button -- Done
+//Create a count using the floating action button -- Done
+//Rotate phone without losing the count -- Done
+//Add a card for displaying counter
+//Add player cards
+//create a counter with +/- buttons on each card
+//Add player name
+//How to delete players?
+//Show score history?
